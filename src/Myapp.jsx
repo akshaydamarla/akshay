f0af5f9501f215edbdf527ed76ad81a1a330c7bb
akshaydamarla <akshaydamarla1@gmail.com>
2025-07-18 10:58:34 +0530

    v3

---
+++ src/Myapp.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Myapp.css';
+class Myapp extends Component {
+    render() {
+        return (
+            <div className='myInfo'>
+                <img className='photo' src='/IMG_4504.PNG' alt='photo' />
+                <table>
+                    <tr>
+                        <td><label>Name</label></td>
+                        <td><label>Akshay Damarla</label></td>
+                    </tr>
+                    <tr>
+                        <td><label>Program</label></td>
+                        <td><label>Btech CSE</label></td>
+                    </tr>
+                    <tr>
+                        <td><label>Mobile Number</label></td>
+                        <td><label>8185935400</label></td>
+                    </tr>
+                    <tr>
+                        <td><label>Email ID</label></td>
+                        <td><label>akshaydamarla1@gmail.com</label></td>
+                    </tr>
+                </table>
+                
+            </div>
+        );
+    }
+}
+
+export default Myapp;
